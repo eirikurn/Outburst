0ddcab23e4ac44e1137e4a2a8b60c60c4011a62b
Ari Þór H. Arnbjörnsson <ari@flassari.is>
2011-08-27 22:34:54 +0300

    Doing some progress, but not working as it should.

--- app.js
+++ app.js
@@ -4,9 +4,20 @@
  */
 
 var express = require('express')
-  , nko = require('nko')('Vzhctm/pgoeQd99c');
+  , nko = require('nko')('Vzhctm/pgoeQd99c')
+  , OAuth= require('oauth').OAuth;
+
+var oa = new OAuth("https://twitter.com/oauth/request_token",
+        "https://twitter.com/oauth/access_token", 
+        "vg7S2p6MSSUGxsJhDgizMw", "zsrWRYBv3e3KX7emhjAJxuwejOPRo5HfbtL5fiL26Y", 
+        "1.0A", "http://192.168.100.38:8000/game/oauth/callback", "HMAC-SHA1");
 
 var app = module.exports = express.createServer();
+
+
+
+
+
 
 // Configuration
 
@@ -36,6 +47,34 @@ app.get('/', function(req, res){
     title: 'Aranja'
   });
 });
+
+app.get('/oauth/authenticate', function(req, res) {
+  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+    if (error) new Error(error.data)
+    else {
+      req.session.oauth.token = oauth_token
+      req.session.oauth.token_secret = oauth_token_secret
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+    }
+  });
+});
+
+
+app.get('/oauth/callback', function(req, res, next){
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier
+    var oauth = req.session.oauth
+
+    oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+      function(error, oauth_access_token, oauth_access_token_secret, results){
+        if (error) new Error(error)
+        console.log(results.screen_name)
+	   }
+	 );
+  } else
+    next(new Error('No OAuth information stored in the session. How did you get here?'))
+  }
+);
 
 var Server = require('./gameserver/main').Server
   , server = new Server(app);

5ddf376ace5f562384aa032fec86d0c12cb2ad1b
Ægir Þorsteinsson <aegir@thorsteinsson.is>
2012-05-08 20:47:45 +0000

    Refactored server.js.

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var coffee = require('coffee-script')
   , app = require('./app')
   , port = process.env.PORT || 8000;
 
-app.listen(process.env.PORT || 8000);
-console.log("Express server listening on port %d in %s mode", port, app.settings.env);
-
+app.listen(port);
+console.log("Listening on port %d in %s mode", port, app.settings.env);
